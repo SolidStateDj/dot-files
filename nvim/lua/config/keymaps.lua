8e6c3614e99198ab157ceca4faa98476145f582d
Madeline Orr <madeline@Madelines-MacBook-Pro.local>
2026-09-23 14:10:22 -0400

    Move up/down fixes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,8 +3,8 @@
 -- Add any additional keymaps here
 
 -- Move selected lines up/down.
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down", silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up", silent = true })
 
 -- Esc & Ctrl+C same behaviour
 vim.keymap.set("i", "<C-c>", "<Esc>")
